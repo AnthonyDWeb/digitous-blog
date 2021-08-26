0ba07190ebbd036c893c114a9876891caf90e8fe
AnthonyDWeb <delforge.anthony@gmail.com>
2021-08-26 10:17:33 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.mainTitle}>Login</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainTitle:{
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+});
